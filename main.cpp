17cbe16050b5422b05e9a4f7ccf9c14e8cca9ba8
KobayashiTokiya <k024g0090@g.neec.ac.jp>
2025-04-23 14:59:42 +0900

    スワップチェーンの生成が出来ただけ

--- main.cpp
+++ main.cpp
@@ -5,6 +5,33 @@
 #include <filesystem>
 #include <fstream>
 #include <chrono>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+#include <dbghelp.h>
+#include <strsafe.h>
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+#pragma comment(lib,"Dbghelp.lib")
+
+static LONG WINAPI ExportDump(EXCEPTION_POINTERS* exception)
+{
+	SYSTEMTIME time;
+	GetLocalTime(&time);
+	wchar_t filePath[MAX_PATH] = { 0 };
+	CreateDirectory(L"./Dumps", nullptr);
+	StringCchPrintfW(filePath, MAX_PATH, L"./Dumps/%04d_%02d%02d_%02d%02d.dmp", time.wYear, time.wMonth, time.wDay, time.wHour, time.wMinute);
+	HANDLE DumpFileHandle = CreateFile(filePath, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE | FILE_SHARE_READ, 0, CREATE_ALWAYS, 0, 0);
+	DWORD processId = GetCurrentProcessId();
+	DWORD threadId = GetCurrentThreadId();
+	MINIDUMP_EXCEPTION_INFORMATION minidumpInformation{ 0 };
+	minidumpInformation.ThreadId = threadId;
+	minidumpInformation.ExceptionPointers = exception;
+	minidumpInformation.ClientPointers = TRUE;
+	MiniDumpWriteDump(GetCurrentProcess(), processId, DumpFileHandle, MiniDumpNormal, &minidumpInformation, nullptr, nullptr);
+	return EXCEPTION_EXECUTE_HANDLER;
+}
 
 std::wstring ConvertString(const std::string& str) {
 	if (str.empty()) {
@@ -54,6 +81,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
+	SetUnhandledExceptionFilter(ExportDump);
+
 	std::filesystem::create_directory("logs");
 
 	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
@@ -62,6 +91,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	std::string dateString = std::format("{:%Y%m%d_%H%M%S}", localTime);
 	std::string logFilePath = std::string("logs/") + dateString + ".log";
 	std::ofstream logStream(logFilePath);
+
+	//uint32_t* p = nullptr;
+	//*p = 100;
 
 	WNDCLASS wc{};
 	wc.lpfnWndProc = WindowProc;
@@ -92,7 +124,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		nullptr);
 
 	ShowWindow(hwnd, SW_SHOW);
-	Log(logStream,"Hello,DirectX!\n");
+	Log(logStream, "Hello,DirectX!\n");
 	Log(logStream,
 		ConvertString
 		(
@@ -105,15 +137,94 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		)
 	);
 
-	MSG msg{};
-	while (msg.message!=WM_QUIT)
+	//DXGIFactoryの生成
+	IDXGIFactory7* dxgiFactory = nullptr;
+	HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
+	assert(SUCCEEDED(hr));
+
+	//使用するアダプタ(GPU)を決定する
+	IDXGIAdapter4* useAdarter = nullptr;
+
+	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i,
+		DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdarter)) != 
+		DXGI_ERROR_NOT_FOUND; ++i)
 	{
-		if (PeekMessage(&msg,NULL,0,0,PM_REMOVE))
+		DXGI_ADAPTER_DESC3 adapterDesc{};
+		hr = useAdarter->GetDesc3(&adapterDesc);
+		assert(SUCCEEDED(hr));
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE))
+		{
+			Log(
+				logStream,
+				ConvertString(
+					std::format(
+						L"Use Adapater:{}\n", adapterDesc.Description
+					)
+				)
+			);
+			break;
+		}
+		useAdarter = nullptr;
+	}
+	assert(useAdarter != nullptr);
+
+	ID3D12Device* device = nullptr;
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0
+	};
+	const char* featureLevelStrings[] = { "12.2","12.1","12.0" };
+	for (size_t i = 0; i < _countof(featureLevels); ++i)
+	{
+		hr = D3D12CreateDevice(useAdarter, featureLevels[i], IID_PPV_ARGS(&device));
+		if (SUCCEEDED(hr))
+		{
+			Log(
+				logStream,
+				std::format("FeatureLevel : {}\n", featureLevelStrings[i]));
+			break;
+		}
+	}
+
+	assert(device != nullptr);
+	Log(logStream, "Complete create D3D12Device!!!\n");
+
+	ID3D12CommandQueue* commandQueue = nullptr;
+	D3D12_COMMAND_QUEUE_DESC commandQueueDesc{};
+	hr = device->CreateCommandQueue(&commandQueueDesc, IID_PPV_ARGS(&commandQueue));
+	assert(SUCCEEDED(hr));
+
+	ID3D12CommandAllocator* CommandAllocator = nullptr;
+	hr = device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&CommandAllocator));
+
+	assert(SUCCEEDED(hr));
+
+	ID3D12GraphicsCommandList* commandList = nullptr;
+	hr = device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, CommandAllocator, nullptr, IID_PPV_ARGS(&commandList));
+	assert(SUCCEEDED(hr));
+
+	//スワップチェーン生成
+	IDXGISwapChain4* swapChain = nullptr;
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
+	swapChainDesc.Width = kClientWindth;
+	swapChainDesc.Height = kClientHeight;
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = 2;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+
+	hr = dxgiFactory->CreateSwapChainForHwnd(commandQueue, hwnd, &swapChainDesc, nullptr, nullptr, reinterpret_cast<IDXGISwapChain1**>(&swapChain));
+	assert(SUCCEEDED(hr));
+
+	MSG msg{};
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
-		}
-		else
+		} else
 		{
 
 		}
