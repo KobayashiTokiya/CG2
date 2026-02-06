e9430079643257d7837810b45fd693fd43ae3151
KobayashiTokiya <k024g0090@g.neec.ac.jp>
2026-02-06 10:23:01 +0900

    04_05できた

--- project/DirectXCommon.cpp
+++ project/DirectXCommon.cpp
@@ -763,4 +763,22 @@ void DirectXCommon::UpdateFixFPS()
 	const std::chrono::microseconds kMinTime(uint64_t(1000000.0f / 60.0f));
 	//1/60秒よりわずかに短い時間
 	const std::chrono::microseconds kMinCheckTime(uint64_t(1000000.0f / 65.0f));
+
+	//現在時間を取得する
+	std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+	//前回記録からの経過時間を取得する
+	std::chrono::microseconds elapsed = std::chrono::duration_cast<std::chrono::microseconds>(now - referece_);
+
+	//1/60(よりわずかに短い時間)経っていない場合
+	if (elapsed<kMinCheckTime)
+	{
+		//1/60秒経過するまで微小スリープを繰り返す
+		while (std::chrono::steady_clock::now()-referece_<kMinTime)
+		{
+			//1マイクロ秒スリープ
+			std::this_thread::sleep_for(std::chrono::microseconds(1));
+		}
+	}
+	//現在の時間を記録する
+	referece_ = std::chrono::steady_clock::now();
 }
