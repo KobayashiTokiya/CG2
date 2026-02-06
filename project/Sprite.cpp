6cc2f9e5a2715ad803044abb50952fd2aba3128a
KobayashiTokiya <k024g0090@g.neec.ac.jp>
2026-02-06 12:54:29 +0900

    頂点データを書いた所

--- project/Sprite.cpp
+++ project/Sprite.cpp
@@ -1,6 +1,8 @@
 #include "Sprite.h"
+#include "SpriteCommon.h"
 
-void Sprite::Initialize()
+void Sprite::Initialize(SpriteCommon* spriteCommon)
 {
-
+	//引数で受け取ってメンバ変数に記録する
+	this->spriteCommon = spriteCommon;
 }
--- project/Sprite.h
+++ project/Sprite.h
@@ -1,11 +1,24 @@
 #pragma once
+#include "main.cpp"
+
+class  SpriteCommon;
+
+//頂点データ
+struct VertexData
+{
+	Vector4 position;
+	Vector2 texcoord;
+	Vector3 normal;
+};
 
 //スプライト
 class Sprite
 {
 public:// メンバ関数
 	//初期化
-	void Initialize();
+	void Initialize(SpriteCommon* spriteCommon);
 
+private:
+	SpriteCommon* spriteCommon = nullptr;
 };
 
