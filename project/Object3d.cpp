ab298146e4784881702b4de13f3e7a6be5a97cd6
kobayashiTokiya <k024g0090@g.neec.ac.jp>
2026-03-04 13:06:25 +0900

    とりあえず座標変換行列の初期化の所まで書き終わりました。 なので次は、平行光源からです。 エラーはあります

--- project/Object3d.cpp
+++ project/Object3d.cpp
@@ -8,6 +8,9 @@ void Object3d::Initialize(Object3dCommon* object3dCommon)
 
 	//モデル読み込み
 	modelData = LoadObjFile("Resource", "plane.obj");
+
+	//頂点データを作成している関数を呼び出す
+	CreateVertexData();
 }
 
 void Object3d::Update()
@@ -18,4 +21,51 @@ void Object3d::Update()
 void Object3d::Draw()
 {
 
+}
+
+void Object3d::CreateVertexData()
+{
+	DirectXCommon* dxCommon = object3dCommon->GetDxCommon();
+
+	//VertexResourceを作る
+	vertexResource = dxCommon->CreateBufferResource(sizeof(VertexData) * 6);
+
+	//VertexBufferViewを作成する(値を設定するだけ)
+	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
+	vertexBufferView.SizeInBytes = sizeof(VertexData)*6;
+	vertexBufferView.StrideInBytes = sizeof(VertexData);
+
+	//VertexResourceにデータを書き込むためのアドレスを取得してvertexDataに割り当てる
+	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+}
+
+void Object3d::CreateMaterialData()
+{
+	DirectXCommon* dxCommon = object3dCommon->GetDxCommon();
+
+	//マテリアルリソースを作る
+	materialResource = dxCommon->CreateBufferResource(sizeof(Material));
+
+	//マテリアルリソースにデータを書き込むためのアドレスを取得してmaterialDataに割り当てる
+	materialResource->Map(0,nullptr,reinterpret_cast<void**>(&materialData));
+
+	//マテリアルデータの初期値を書き込む
+	materialData->color = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+	materialData->enableLighting = false;
+	materialData->uvTransform = MatrixMath::MakeIdentity4x4();
+}
+
+void Object3d::CreateTransformationData()
+{
+	DirectXCommon* dxCommon = object3dCommon->GetDxCommon();
+
+	//座標変換行列リソースを作る
+	transformationResource = dxCommon->CreateBufferResource(sizeof(TransformationMatrix));
+
+	//座標変換行列リソースにデータを書き込むためのアドレスを取得してtransformationMatrixDataに割り当てる
+	transformationResource->Map(0, nullptr, reinterpret_cast<void**>(&transformationMatrixData));
+
+	//単位行列を書き込んでおく
+	transformationMatrixData->WVP = MatrixMath::MakeIdentity4x4();
+	transformationMatrixData->World = MatrixMath::MakeIdentity4x4();
 }
--- project/Object3d.h
+++ project/Object3d.h
@@ -2,6 +2,8 @@
 #include "WinApp.h"
 #include <Matrix.h>
 
+#include <d3d12.h>
+#include <wrl.h>
 
 class Object3dCommon;
 
@@ -24,6 +26,22 @@ struct ModelData
 	MaterialData material;
 };
 
+//マテリアルデータ
+struct Material
+{
+	Vector4 color;
+	int32_t enableLighting;
+	float padding[3]; // パディング（サイズ調整）
+	Matrix4x4 uvTransform;
+};
+
+//座標変換行列データ
+struct  TransformationMatrix
+{
+	Matrix4x4 WVP;
+	Matrix4x4 World;
+};
+
 //3Dオブジェクト
 class Object3d
 {
@@ -42,16 +60,43 @@ public://メンバ関数
 	static ModelData LoadObjFile(const std::string& directoryPath, const std::string& filename);
 
 private://
+	//頂点データ作成用の関数
+	void CreateVertexData();
+
+	//マテリアルデータ作成用の関数
+	void CreateMaterialData();
+
+	//座標変換行列作成用の関数
+	void CreateTransformationData();
+
+private://
 	Object3dCommon* object3dCommon = nullptr;
 
 	//Objファイルのデータ
 	ModelData modelData;
 
+#pragma region 頂点データ
 	//バッファリソース
-	
+	Microsoft::WRL::ComPtr<ID3D12Resource>vertexResource;
 	//バッファリソース内のデータを指すポインタ
 	VertexData* vertexData = nullptr;
 	//バッファリソースの使い道を補足するバッファビュー
 	D3D12_VERTEX_BUFFER_VIEW vertexBufferView;
+#pragma endregion
+
+#pragma region マテリアル
+	//バッファリソース（マテリアル用）
+	Microsoft::WRL::ComPtr<ID3D12Resource> materialResource;
+	//バッファリソース内のデータを指すポインタ
+	Material* materialData = nullptr;
+#pragma endregion
+
+#pragma region 座標変換行列
+	//バッファリソース(座標変換行列用)
+	Microsoft::WRL::ComPtr<ID3D12Resource>transformationResource;
+	//バッファリソース内のデータを指すポインタ
+	TransformationMatrix* transformationMatrixData = nullptr;
+#pragma endregion
+
 };
 
