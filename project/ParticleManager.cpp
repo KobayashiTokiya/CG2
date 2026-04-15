6ef6352f2b92158c8a3fc71bc65d79045e858806
kobayashiTokiya <k024g0090@g.neec.ac.jp>
2026-04-15 15:21:30 +0900

    CG3のブレンドを入れたのとパイプライン、ルートシグネチャをObject3dから持ってきただけなのでエラーになっている。 次に、そのエラーを消していく。

--- project/ParticleManager.cpp
+++ project/ParticleManager.cpp
@@ -14,4 +14,186 @@ void ParticleManager::Initialize(DirectXCommon* dxCommon,SrvManager* srvManager)
 	//ランダムエンジンの初期化
 	std::random_device seedGenerator;
 	randomEngine_.seed(seedGenerator());
+
+	CreateRootSignature();
+	CreateGraphicsPipelineState();
+
+	for (int  i = 0; i < static_cast<int>(BlendMode::kCountOfBlendMode); ++i)
+	{
+		//モードに応じたブレンド設定をセット
+	
+
+	}
+}
+
+
+	
+void ParticleManager::CreateRootSignature()
+{
+	ID3D12Device* device = dxCommon_->GetDevice();
+
+	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
+	descriptionRootSignature.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+
+	D3D12_DESCRIPTOR_RANGE descriptorRange[1] = {};
+	descriptorRange[0].BaseShaderRegister = 0; // t0
+	descriptorRange[0].NumDescriptors = 1;
+	descriptorRange[0].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+	descriptorRange[0].OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+
+	// ★パーティクル用に RootParameter を 2つ にスッキリさせます
+	D3D12_ROOT_PARAMETER rootParameters[2] = {};
+
+	// 0番: 定数バッファ (CBV)用
+	rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+	rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
+	rootParameters[0].Descriptor.ShaderRegister = 0;
+
+	// 1番: テクスチャ (SRV)用
+	rootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
+	rootParameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+	rootParameters[1].DescriptorTable.pDescriptorRanges = descriptorRange;
+	rootParameters[1].DescriptorTable.NumDescriptorRanges = _countof(descriptorRange);
+
+	descriptionRootSignature.pParameters = rootParameters;
+	descriptionRootSignature.NumParameters = _countof(rootParameters);
+
+	// サンプラーの設定
+	D3D12_STATIC_SAMPLER_DESC staticSamplers[1] = {};
+	staticSamplers[0].Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
+	staticSamplers[0].AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	staticSamplers[0].AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	staticSamplers[0].AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	staticSamplers[0].ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER;
+	staticSamplers[0].MaxLOD = D3D12_FLOAT32_MAX;
+	staticSamplers[0].ShaderRegister = 0; // s0
+	staticSamplers[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+
+	descriptionRootSignature.pStaticSamplers = staticSamplers;
+	descriptionRootSignature.NumStaticSamplers = _countof(staticSamplers);
+
+	ID3DBlob* signatureBlob = nullptr;
+	ID3DBlob* errorBlob = nullptr;
+
+	HRESULT hr = D3D12SerializeRootSignature(&descriptionRootSignature,
+		D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlob);
+
+	assert(SUCCEEDED(hr));
+
+	// GPU上にルートシグネチャを作成
+	hr = device->CreateRootSignature(
+		0,
+		signatureBlob->GetBufferPointer(),
+		signatureBlob->GetBufferSize(),
+		IID_PPV_ARGS(&rootSignature_)); // ★メンバ変数に入れる
+
+	assert(SUCCEEDED(hr));
+
+	if (signatureBlob) { signatureBlob->Release(); }
+	if (errorBlob) { errorBlob->Release(); }
+}
+
+void ParticleManager::CreateGraphicsPipelineState()
+{
+	ID3D12Device* device = dxCommon_->GetDevice();
+
+	// ★Shaderを Particle 用に変更！
+	Microsoft::WRL::ComPtr<IDxcBlob> vertexShaderBlob = dxCommon_->CompileShader(L"Resource/shaders/Particle.VS.hlsl", L"vs_6_0");
+	assert(vertexShaderBlob != nullptr);
+
+	Microsoft::WRL::ComPtr<IDxcBlob> pixelShaderBlob = dxCommon_->CompileShader(L"Resource/shaders/Particle.PS.hlsl", L"ps_6_0");
+	assert(pixelShaderBlob != nullptr);
+
+	// ★インプットレイアウトを Particle 用 (POSITIONのみ) に変更！
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs[1] = {};
+	inputElementDescs[0].SemanticName = "POSITION";
+	inputElementDescs[0].SemanticIndex = 0;
+	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+
+	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
+	inputLayoutDesc.pInputElementDescs = inputElementDescs;
+	inputLayoutDesc.NumElements = _countof(inputElementDescs);
+
+	// RasterizerState
+	D3D12_RASTERIZER_DESC rasterizerDesc{};
+	rasterizerDesc.CullMode = D3D12_CULL_MODE_NONE; // 裏面も描画
+	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
+
+	// ★DepthStencilStateの設定（書き込みを ZERO にする！）
+	D3D12_DEPTH_STENCIL_DESC depthStencilDesc{};
+	depthStencilDesc.DepthEnable = true;
+	depthStencilDesc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ZERO; // パーティクルはZバッファに書き込まない
+	depthStencilDesc.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
+
+	// PSOの共通設定
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC pipelineDesc{};
+	pipelineDesc.pRootSignature = rootSignature_;
+	pipelineDesc.InputLayout = inputLayoutDesc;
+	pipelineDesc.VS = { vertexShaderBlob->GetBufferPointer(), vertexShaderBlob->GetBufferSize() };
+	pipelineDesc.PS = { pixelShaderBlob->GetBufferPointer(), pixelShaderBlob->GetBufferSize() };
+	pipelineDesc.RasterizerState = rasterizerDesc;
+	pipelineDesc.NumRenderTargets = 1;
+	pipelineDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+	pipelineDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	pipelineDesc.SampleDesc.Count = 1;
+	pipelineDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;
+	pipelineDesc.DepthStencilState = depthStencilDesc;
+	pipelineDesc.DSVFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+
+	// ★ここが一番大事！ブレンドモードの数だけループしてパイプラインを作る！
+	for (int i = 0; i < static_cast<int>(BlendMode::kCountOfBlendMode); ++i)
+	{
+		BlendMode mode = static_cast<BlendMode>(i);
+
+		// モードに応じたブレンド設定をセット
+		pipelineDesc.BlendState = GetBlendDesc(mode);
+
+		// パイプラインを配列に保存
+		HRESULT hr = device->CreateGraphicsPipelineState(&pipelineDesc, IID_PPV_ARGS(&graphicsPipelineStates_[i]));
+		assert(SUCCEEDED(hr));
+	}
+}
+
+D3D12_BLEND_DESC ParticleManager::GetBlendDesc(BlendMode mode)
+{
+	D3D12_BLEND_DESC blendDesc{};
+	blendDesc.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+	blendDesc.RenderTarget[0].BlendEnable = TRUE;
+
+	//アルファチャンネル設定(共通)
+	blendDesc.RenderTarget[0].SrcBlendAlpha = D3D12_BLEND_ONE;
+	blendDesc.RenderTarget[0].BlendOpAlpha = D3D12_BLEND_OP_ADD;
+	blendDesc.RenderTarget[0].DestBlendAlpha = D3D12_BLEND_ZERO;
+
+	switch (mode)
+	{
+	case BlendMode::kBlendModeNormal:   //通常
+		blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
+		blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
+		blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
+		break;
+	case BlendMode::kBlendModeAdd:      //加算
+		blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
+	blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
+	blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_ONE;
+		break;
+	case BlendMode::kBlendModeSubtract: //減算
+		blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
+		blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_REV_SUBTRACT;
+		blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_ONE;
+		break;
+	case BlendMode::kBlendModeMultiply: //乗算
+		blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_ZERO;
+		blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
+		blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_SRC_COLOR;
+		break;
+	case BlendMode::kBlendModeScreen:   //スクリーン
+		blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_INV_DEST_COLOR;
+		blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
+		blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_ONE;
+		break;
+	}
+	return blendDesc;
+
 }
--- project/ParticleManager.h
+++ project/ParticleManager.h
@@ -2,10 +2,23 @@
 #include "DirectXCommon.h"
 #include "SrvManager.h"
 #include <random>
+#include <list>
+#include <unordered_map>
+#include <string>
 
 struct VertexParticle
 {
 	Vector4 position;
+};
+
+enum class BlendMode
+{
+	kBlendModeNormal,   // 通常（半透明）
+	kBlendModeAdd,      // 加算合成
+	kBlendModeSubtract, // 減算合成
+	kBlendModeMultiply, // 乗算合成
+	kBlendModeScreen,   // スクリーン合成
+	kCountOfBlendMode   // 総数
 };
 
 class ParticleManager
@@ -23,6 +36,7 @@ private:
 	ParticleManager(const ParticleManager&) = delete;
 	ParticleManager& operator=(const ParticleManager&) = delete;
 
+	D3D12_BLEND_DESC GetBlendDesc(BlendMode mode);
 private:
 	//メンバ変数に記録するためのポインタ
 	DirectXCommon* dxCommon_ = nullptr;
@@ -30,5 +44,13 @@ private:
 
 	//ランダムエンジン
 	std::mt19937 randomEngine_;
+
+private:
+	//ルートシグネチャの作成
+	void CreateRootSignature();
+
+	//グラフィックスパイプラインの生成
+	void CreateGraphicsPipelineState();
+
 };
 
